991667ca7cc88ca5d5fd7c5f70b52948e96a6843
Divya-Dandagavhal <83975998+Divya-Dandagavhal@users.noreply.github.com>
2023-03-31 14:42:35 +0530

    Add files via upload

---
+++ Credit_Card_Fraud/app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='Value = {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+import pickle
+
+
+with open('serialized.pkl', 'rb') as f:
+    data = pickle.load(f)
